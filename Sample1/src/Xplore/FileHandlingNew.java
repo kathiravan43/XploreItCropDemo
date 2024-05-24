16859b2b0e460314453c6a2324065b5724f8d70e
kathiravan43 <kathiravanlax49@gmail.com>
2024-05-24 10:35:24 +0530

    updated

--- Sample1/src/Xplore/FileHandlingNew.java
+++ Sample1/src/Xplore/FileHandlingNew.java
@@ -1,8 +1,44 @@
 package Xplore;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
+
+class FileWriterDemo{
+	 final static String fileName="C:\\Users\\ADMIN\\OneDrive\\Desktop\\FileHandling\\Data1.txt";
+	public  static void towrite() {
+		try {
+			FileWriter fileWriter = new FileWriter(fileName);
+			fileWriter.write("Hello welcome to file handling topic");
+			fileWriter.close();
+			System.out.println("Wrote successfully");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
+class FileReaderDemo extends FileWriterDemo{
+	public static void toRead() {
+		try {
+		File file = new File(FileWriterDemo.fileName);
+		Scanner scanner = new Scanner(file);
+		while(scanner.hasNextLine()) {
+		String data = scanner.nextLine();
+		System.out.println("Data from file : "+data);
+		}
+		}catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+
+	
+	
+}
 public class FileHandlingNew {
 
 	public static void main(String[] args) {
@@ -12,13 +48,25 @@ public class FileHandlingNew {
 			{
 				System.out.println("File Created : "+file.getName());
 			}
+			else if(file.exists())
+			{
+				System.out.println("Name of the File : "+file.getName());
+				System.out.println("Absolute file path : "+file.getAbsolutePath());
+				System.out.println("Wriatable : "+file.canWrite());
+				System.out.println("Readable : "+file.canRead());
+				System.out.println("File size in the byte : "+ file.length());
+			}
+			
 			else {
 				System.out.println("The Already exist.....!");
 			}
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		FileWriterDemo.towrite();
+		FileReaderDemo.toRead();
 
 	}
 
